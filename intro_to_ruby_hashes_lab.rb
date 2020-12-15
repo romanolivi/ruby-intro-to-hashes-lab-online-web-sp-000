69928945a1cffc21c05c6f97d319ae48885c57a5
Romano Livi <romano_livi@outlook.com>
2020-12-15 16:21:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :names => {
-        :reading_railroad => {"mortgage_value"},
+        :reading_railroad => {"mortgage_value" =>},
         :pennsylvania_railroad => {"mortgage_value"},
         :b_and_o_railroad => {"mortgage_value" },
         :shortline => {"mortgage_value"}
